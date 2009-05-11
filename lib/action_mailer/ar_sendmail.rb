7a659fce06c6da6efb78bed871e4efb5c32bd923
David Palm <dpalm@elctech.com>
2009-05-11 17:46:08 +0200

    Added success_status to emails table template Storing Net::SMTP::Response#string in Email#success_status so we keep a reference to the smtp server mail ID too

--- lib/action_mailer/ar_sendmail.rb
+++ lib/action_mailer/ar_sendmail.rb
@@ -113,6 +113,7 @@ class Create#{table_name.classify} < ActiveRecord::Migration
       t.column :mail, :text
       t.column :last_send_attempt, :integer, :default => 0
       t.column :last_error, :text
+      t.column :success_status, :string
       t.column :attempts, :integer
       t.column :failed, :boolean, :default => false
       t.column :created_at, :datetime
@@ -466,6 +467,7 @@ end
           res = session.send_message email.mail, email.from, email.to
           email.failed = false
           email.sent_at = Time.now
+          email.success_status = res.string
           
           log "#{self.class}#deliver sent email %011d from %s to %s: %p" %
                 [email.id, email.from, email.to, res]
